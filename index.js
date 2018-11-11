89521e4e7d079dc896b3b0daf271cb441024e342
Tunmise Ogunniyi <ogunniyitunmise@gmail.com>
2018-11-11 21:38:20 +0100

    Portal cli version 1.0

--- index.js
+++ index.js
@@ -1,1 +1,60 @@
-console.log('I am Andela Portal CMD');
+#!/usr/bin/env node
+
+var program = require('commander');
+const { exec } = require('child_process');
+const readline = require('readline');
+var commandExistsSync = require('command-exists').sync;
+const portals = require('./resource.json');
+const pkg = require('./package.json');
+
+if (!commandExistsSync('open')) {
+  console.log(`Sorry, "apl" can't work in this shell environment.`);
+  process.exit();
+}
+
+const portalNames = Object.keys(portals).sort();
+
+const listPortals = () => {
+  portalNames.forEach((name) =>  {
+    console.log(`=> ${name}: ${portals[name]}`);
+  });
+};
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+program
+  .version(`${pkg.version}`, '-v, --version')
+  .arguments('<portalName>')
+  .action((portalName) => {
+    portalName = portalName.toLocaleLowerCase();
+    if(!portalNames.includes(`${portalName.toLocaleLowerCase()}`)) 
+      return console.log(`"${portalName}" is not a recognised portal.`);
+
+    console.log(`Opening ${portals[portalName]}`)
+    exec(`open ${portals[portalName]}`, (error) => {
+      if (error) return console.error(`exec error: ${error}`);
+
+      console.log('Done!!');
+      process.exit()
+    });
+  });
+
+program
+  .option('-l, --list', 'list all portals', () => {
+    const portalLength = portalNames.length;
+    if (portalLength <= 20) return listPortals();
+
+    rl.question(`Are you sure you want to view all ${portalNames.length} portals?(y/n) `, (answer) => {
+      if (answer.toLowerCase() === 'y' || answer.toLocaleLowerCase === 'yes') {
+        rl.close();
+        listPortals();
+      } else {
+        rl.close();
+      }
+    });
+  });
+
+program.parse(process.argv);
