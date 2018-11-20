952b7c82e860a0fe9510ceddfe1bebe164c99874
Tunmise Ogunniyi <ogunniyitunmise@gmail.com>
2018-11-20 12:18:04 +0100

    Fix the command check logic for different types of OS

--- index.js
+++ index.js
@@ -8,23 +8,27 @@ var commandExistsSync = require('command-exists').sync;
 const portals = require('./resource.json');
 const pkg = require('./package.json');
 
-if (!commandExistsSync('open')) {
+const systemCommand = {
+  Darwin: 'open',
+  Linux: 'firefox',
+  Windows_NT: 'start'
+}
+
+const systemType = os.type();
+
+if (
+  (systemType === 'Linux' && !commandExistsSync('firefox')) ||
+  (systemType === 'Windows_NT' && !commandExistsSync('start')) ||
+  (systemType === 'Darwin' && !commandExistsSync('open'))
+) {
   console.log(`Sorry, "apl" can't work in this shell environment.`);
   process.exit();
 }
 
 const portalNames = Object.keys(portals).sort();
 
-const systemCommand = {
-    Darwin: 'open',
-    Linux: 'firefox',
-    Windows_NT: 'start'
-}
-
-const systemType = os.type();
-
 const listPortals = () => {
-  portalNames.forEach((name) =>  {
+  portalNames.forEach((name) => {
     console.log(`=> ${name}: ${portals[name]}`);
   });
 };
@@ -39,15 +43,15 @@ program
   .arguments('<portalName>')
   .action((portalName) => {
     portalName = portalName.toLocaleLowerCase();
-    if(!portalNames.includes(`${portalName.toLocaleLowerCase()}`)) 
+    if(!portalNames.includes(`${portalName.toLocaleLowerCase()}`))
       return console.log(`"${portalName}" is not a recognised portal.`);
-
+    
     console.log(`Opening ${portals[portalName]}`)
     exec(`${systemCommand[systemType]} ${portals[portalName]}`, (error) => {
       if (error) return console.error(`exec error: ${error}`);
 
       console.log('Done!!');
-      process.exit()
+      process.exit();
     });
   });
 
@@ -66,10 +70,10 @@ program
     });
   });
 
-  const optionsLength = process.argv.length;
-  
-  if (optionsLength === 2) {
-    program.help();
-  }
+const optionsLength = process.argv.length;
+
+if (optionsLength === 2) {
+  program.help();
+}
 
 program.parse(process.argv);
